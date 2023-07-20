59776276f447ce1d4a37935622ccabe3abddcb74
Ellie Barrera <elliediaz1204@gmail.com>
2023-07-20 10:35:50 -0400

    added email trigger for restaurant

--- routes/Stripe.js
+++ routes/Stripe.js
@@ -80,9 +80,10 @@ const createOrder = async (customer, data) => {
         from: process.env.EMAIL_USER,
         to: data.customer_details.email,
         subject: 'Thank you for your purchase with Pops Pizzeria!',
-        text: `Thank you for your Purchase with Pops Pizzeria! your Order ID is: ${data.id}. 
-              Your order might take some time to process.
-              If you have any questions please contact us at Tel: (631) 736-3957 `
+        html: `<p>Thank you for your Purchase with Pops Pizzeria!</p> 
+              <p>Your Order ID is: ${data.id}.</p>
+              <p>Your order might take some time to process.</p>
+              <p>If you have any questions, please contact us at Tel: (631) 736-3957.</p>`
       };
 
       transporter.sendMail(mailOptions, function(error, info){
@@ -93,7 +94,7 @@ const createOrder = async (customer, data) => {
         }
       });
 
-      //email Pizzeria (need to customize for JJ)
+      //email Pizzeria (need to customize)
       var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -102,14 +103,34 @@ const createOrder = async (customer, data) => {
         }
       });
 
+      // Create a variable to store the email body
+      let emailBody = `<p>Customer name: ${data.customer_details.name}</p>
+      <p>Customer Phone Number: ${data.customer_details.phone}</p>
+      <p>Order Type: Delivery or pick up</p>
+      <p>Order Details:</p>
+      <ul>`;
+        // Loop through the products and append the product details to the email body
+        Items.forEach((product) => {
+        const { name, quantity, price, selectedOptions } = product;
+        emailBody += `
+        <li>Item Name: ${name}</li>
+        <li>Quantity: ${quantity}</li>
+        <li>Item Price: $${(price)}</li>
+        <li>Choice Of: ${selectedOptions || 'N/A'}</li>
+        <br>`;
+        });
+        emailBody += `<h3>Total: $${(savedOrder.total / 100).toFixed(2)}</h3>`;
+        emailBody += `<h3>Payment Status: ${data.payment_status}</h3>
+        <p>Time Order was created: ${savedOrder.createdAt}</p>
+      </ul>`;
+
+
       var mailOptions = {
         from: process.env.EMAIL_USER,
         to: process.env.EMAIL_TEST,
         subject: 'NEW INCOMING ORDER',
-        text: ` Customer name: ${data.customer_details.name},
-               Customer Phone Number: ${data.customer_details.phone},
-               Order Type: Delivery or pick up, 
-               Order Details: ${savedOrder}`
+        html: emailBody
+
       };
 
       transporter.sendMail(mailOptions, function(error, info){
